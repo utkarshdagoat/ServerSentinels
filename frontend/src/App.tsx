1d6448c6aae28a00202db1144fd24f8b910992c2
pranav <pranavpardeshi24@gmail.com>
2023-10-28 05:06:20 +0530

    implements navigation and login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css'
+import Login from './components/login';
 
 // import { Button } from "@nextui-org/react";
 // import NavBar from "./components/navbar";
 import Landing from "./pages/landing";
+import {  Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="bg-gradient-to-r from-[#99003c] via-[#2d2d66] to-[#2c7fe5]">
-      <Landing />
+    <Routes>
+      <Route path="/" Component={Landing}/>
+      <Route path="/login" Component={Login}/>
+    </Routes>
     </div>
   );
 }
