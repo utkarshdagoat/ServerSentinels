6e72bf3e5a92a902bfb7779ca1a895851500b747
Amrit3985 <amritchirania@gmail.com>
2023-10-28 20:08:45 +0530

    Refactors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,11 @@ import './App.css'
 
 // import { Button } from "@nextui-org/react";
 // import NavBar from "./components/navbar";
-import Landing from "./pages/landing";
-import Carousel from "./components/Carousel"
-import Latestupdates from './components/Latestupdates';
-import Mostpopular from './components/Mostpopular';
-import TopRated from './components/TopRated';
-import Footer from './components/Footer';
 
+import Homepage from './pages/Homepage';
 function App() {
   return (
-     <div className="bg-gradient-to-r from-[#99003c] via-[#2d2d66] to-[#2c7fe5]">
-      <Landing />
-      <Carousel />
-      <Latestupdates />
-      <Mostpopular />
-      <TopRated />
-      <Footer />
-    </div> 
+     <Homepage />
     
   );
 }
---
+++ frontend/src/pages/Homepage.tsx
@@ -0,0 +1,22 @@
+import Landing from "./landing";
+import Carousel from "../components/Carousel";
+import Latestupdates from '../components/Latestupdates';
+import Mostpopular from '../components/Mostpopular';
+import TopRated from '../components/TopRated';
+import Footer from '../components/Footer';
+
+function Homepage() {
+  return (
+     <div className="bg-gradient-to-r from-[#99003c] via-[#2d2d66] to-[#2c7fe5]">
+      <Landing />
+      <Carousel />
+      <Latestupdates />
+      <Mostpopular />
+      <TopRated />
+      <Footer />
+    </div> 
+    
+  );
+}
+
+export default Homepage;
