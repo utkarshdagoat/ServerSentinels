83a2f7cad7632d0bd4a0d71ecf6f243497a8b41f
utkarshdagoat <utkarsh382004@gmail.com>
2023-10-28 20:25:17 +0545

    Login , manga creation , fully Functional

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,28 @@ import Login from './components/login';
 
 // import { Button } from "@nextui-org/react";
 // import NavBar from "./components/navbar";
+<<<<<<< HEAD
 import Landing from "./pages/landing";
 import {  Route, Routes } from 'react-router-dom';
 function App() {
   return (
 
+=======
+import { Routes , Route } from 'react-router-dom';
+
+import Homepage from './pages/Homepage';
+function App() {
+  return (
+    <div className="bg-gradient-to-r from-[#99003c] via-[#2d2d66] to-[#2c7fe5]">
+>>>>>>> 54744d2 (Login , manga creation , fully Functional)
     <Routes>
-      <Route path="/" Component={Landing}/>
+      <Route path="/" Component={Homepage}/>
       <Route path="/login" Component={Login}/>
     </Routes>
+<<<<<<< HEAD
+=======
+    </div>
+>>>>>>> 54744d2 (Login , manga creation , fully Functional)
   );
 }
 
