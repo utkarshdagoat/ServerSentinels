54744d2f9fa072cb270d075b452dfdc3c0047005
utkarshdagoat <utkarsh382004@gmail.com>
2023-10-28 20:25:17 +0545

    Login , manga creation , fully Functional

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,30 +3,17 @@ import Login from './components/login';
 
 // import { Button } from "@nextui-org/react";
 // import NavBar from "./components/navbar";
-<<<<<<< HEAD
-=======
-import Landing from "./pages/landing";
-import {  Route, Routes } from 'react-router-dom';
->>>>>>> 1d6448c (implements navigation and login)
+import { Routes , Route } from 'react-router-dom';
 
 import Homepage from './pages/Homepage';
 function App() {
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-     <Homepage />
-    
-=======
     <div className="bg-gradient-to-r from-[#99003c] via-[#2d2d66] to-[#2c7fe5]">
-=======
-    <div className="bg-gradient-to-r from-[#99003c] via-[#2d2d66] to-[#2c7fe5]" id="main">
->>>>>>> fe3c331 (Login , manga creation , fully Functional)
     <Routes>
-      <Route path="/" Component={Landing}/>
+      <Route path="/" Component={Homepage}/>
       <Route path="/login" Component={Login}/>
     </Routes>
     </div>
->>>>>>> 1d6448c (implements navigation and login)
   );
 }
 
